8d6b747c6884786cf335d80d43b6cc24f6bee871
Mohammad Amin <ghorbannezhadm2@gmail.com>
2023-12-08 14:39:14 +0330

    adding the test file

---
+++ logic/pytest.py
@@ -0,0 +1,34 @@
+import unittest
+
+from logic.dfa import Dfa
+
+
+class TestDfaMethods(unittest.TestCase):
+    def setUp(self):
+        self.dfa = Dfa(
+            states=['q0', 'q1', 'q2'],
+            alphabet=['a', 'b'],
+            transitions={
+                ('q0', 'a'): 'q1',
+                ('q0', 'b'): 'q0',
+                ('q1', 'a'): 'q2',
+                ('q1', 'b'): 'q1',
+                ('q2', 'a'): 'q2',
+                ('q2', 'b'): 'q2'
+            },
+            start_state='q0',
+            accept_states=['q2']
+        )
+
+    def test_accepts_string(self):
+        self.assertTrue(self.dfa.accepts_string('abab'))
+        self.assertFalse(self.dfa.accepts_string('bbbb'))
+
+    def test_minimize(self):
+        self.assertEqual(self.dfa.minimize(), self.dfa)
+        self.assertTrue(self.dfa.minimize().accepts_string('abab'))
+        self.assertFalse(self.dfa.minimize().accepts_string('bbbb'))
+
+
+if __name__ == '__main__':
+    unittest.main()
