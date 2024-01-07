b32ff05dcfbb50462402c106caa19ccd1e0203da
Mohammad Amin <ghorbannezhadm2@gmail.com>
2024-01-07 12:49:49 +0330

    Update pda.py

--- logic/pda.py
+++ logic/pda.py
@@ -8,19 +8,6 @@ class PDA:
         self.accept_states = accept_states
         self.stack = [stack_start_symbol]
 
-    def accept_string(self, input_string):
-        """
-        Checks if the PDA accepts a given input string.
-
-        Args:
-        - input_string: The input string to check
-
-        Returns:
-        - True if the PDA accepts the input string, False otherwise
-        """
-        # Implement the logic to check if the PDA accepts the input string
-        pass
-
     def display_accepted_strings(self, count=10):
         """
         Displays 'count' number of strings accepted by the PDA.
@@ -51,3 +38,15 @@ class PDA:
         # Implement logic to display the changes in the stack during processing the input string
         pass
 
+    def accept_string(self, input_string):
+        """
+        Checks if the PDA accepts a given input string.
+
+        Args:
+        - input_string: The input string to check
+
+        Returns:
+        - True if the PDA accepts the input string, False otherwise
+        """
+        # Implement the logic to check if the PDA accepts the input string
+        pass
